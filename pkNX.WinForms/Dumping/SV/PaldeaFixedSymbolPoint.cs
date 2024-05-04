86ac8ce87c9cc25410c320bb25c6dc32fcff48b7
Minh Phuong Nguyen <minhphuong0401@live.com>
2024-05-05 00:09:27 +1000

    Update

--- pkNX.WinForms/Dumping/SV/PaldeaFixedSymbolPoint.cs
+++ pkNX.WinForms/Dumping/SV/PaldeaFixedSymbolPoint.cs
@@ -11,4 +11,17 @@ public class PaldeaFixedSymbolPoint(string key, PackedVec3f pos)
         Y = pos.Y,
         Z = pos.Z,
     };
+
+    public PaldeaFixedSymbolPoint DeepCopy()
+    {
+        PackedVec3f NewPosition = new PackedVec3f
+        {
+            X = this.Position.X,
+            Y = this.Position.Y,
+            Z = this.Position.Z,
+        };
+        PaldeaFixedSymbolPoint deepcopyPoint = new PaldeaFixedSymbolPoint(this.TableKey, NewPosition);
+
+        return deepcopyPoint;
+    }
 }
